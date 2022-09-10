8e29e971124fe77ea0631866ffab56bd22061287
Evyn Lally <evynlally@gmail.com>
2022-09-09 22:28:19 -0400

    reverted FirstJ

--- docs/FirstJS/firstJS.js
+++ docs/FirstJS/firstJS.js
@@ -5,34 +5,34 @@ myHeading.textContent = 'Magic 8-Ball';
 const myText = document.querySelector('p');
 myText.textContent = 'Please click on a box color and your fortune will appear';
 
-// document.querySelector('#red').addEventListener('click', () => {
-//     alert('you will have a good day');
-// })
+document.querySelector('#red').addEventListener('click', () => {
+    alert('you will have a good day');
+})
 
-// document.querySelector('#yellow').addEventListener('click', () => {
-//     alert('you will have a bad day');
-// })
+document.querySelector('#yellow').addEventListener('click', () => {
+    alert('you will have a bad day');
+})
 
-// document.querySelector('#green').addEventListener('click', () => {
-//     alert('you will be suprised');
-// })
+document.querySelector('#green').addEventListener('click', () => {
+    alert('you will be suprised');
+})
 
-// document.querySelector('#blue').addEventListener('click', () => {
-//     alert('you will be tired');
-// })
+document.querySelector('#blue').addEventListener('click', () => {
+    alert('you will be tired');
+})
 
-const myBox = document.querySelector('#red p');
+// const myBox = document.querySelector('#red p');
 
-myBox.onclick = () => {
-    console.log(myBox);
-    myBox=document.querySelector('p');
-    myBox.classList.add('fortune');
-    // const myFortune = myBox.getAttribute('src');
-    // if (myBox === '#red'){
-    //     myBox.setAttribute('src','.red');
-    // } else{
-    //     myBox.setAttribute('src','#red');
-    // }
-}
+// myBox.onclick = () => {
+//     console.log(myBox);
+//     myBox=document.querySelector('p');
+//     myBox.classList.add('fortune');
+//     const myFortune = myBox.getAttribute('src');
+//     if (myBox === '#red'){
+//         myBox.setAttribute('src','.red');
+//     } else{
+//         myBox.setAttribute('src','#red');
+//     }
+// }
 
 

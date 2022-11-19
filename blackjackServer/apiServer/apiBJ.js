58e9b5eb0d88da6c720f217c7683e616de2db3f8
Evyn Lally <evynlally@gmail.com>
2022-11-18 19:06:53 -0500

    current blackjack

--- blackjackServer/apiServer/apiBJ.js
+++ blackjackServer/apiServer/apiBJ.js
@@ -3,289 +3,236 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-function htmlStart(res, title){
-    res.write(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> ${title} </title>
-    </head>
-    <body>`);
-};
+app.use(express.static(`public`));
+app.use(express.static(`private`));
 
-function htmlEnd(res){
-    res.write(`</body>
-    </html>`);
-    res.end();
-};
 
-    // let deck = [];
-    // i=rank
-    // j=suite
-    // for (let i = 1; i <= 13; i++) {
-    //      for (let j = 1; j <= 4; j++) {
-    //          deck.push({rank:i, suite:j});
-    //         }
-    // }
-    //clubs = 1
-    //diamonds = 2
-    //spades = 3
-    //hearts = 4
-    // return deck;
+// function htmlStart(res, title){
+//     res.write(`<!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title> ${title} </title>
+//     </head>
+//     <body>`);
+// };
+  
+// function htmlEnd(res){
+//     res.write(`</body>
+//     </html>`);
+//     res.end();
+// };
+
+let deck = [];
+let game = {playerHand:[], dealerHand:[], winner: '', message: ''};
 
  function buildDeck() {
 
-        let count = 0;
+    let count = 0;
         
-        for (let i = 1; i <= 13; i++) {
-            deck[count++] = new Card('H', i);
-        }
-        for (let i = 1; i <= 13; i++) {
-            deck[count++] = new Card('S', i);
-        }
-        for (let i = 1; i <= 13; i++) {
-            deck[count++] = new Card('C', i);
-        }
-        for (let i = 1; i <= 13; i++) {
-            deck[count++] = new Card('D', i);
-        }	
+    for (let i = 1; i <= 13; i++) {
+        
+        deck.push({suit: `Hearts`, value: i});
+    }
+    for (let i = 1; i <= 13; i++) {
+        deck.push({suit: `Spades`, value: i});
+    }
+    for (let i = 1; i <= 13; i++) {
+        deck.push({suit: `Clubs`, value: i});
+    }
+    for (let i = 1; i <= 13; i++) {
+        deck.push({suit: `Diamonds`, value: i});
+    }	
  }    
- 
-    
-function toString() {
-    return getSuitName() + " " + this.value;
-}
-    
-function getSuitName() {
-        let suit;   
-        if (this.suit == 'H') {
-            suit = "Hearts";    
-        }
-        else if (this.suit == 'S') {    
-            suit = "Spades";    
-        }
-        else if (this.suit == 'C') {   
-            suit = "Clubs";   
-        }
-        else if (this.suit == 'D') {   
-            suit = "Diamonds";
-    
-        } else {
-           suit = "Unknown";
-        }
         
-            return suit;
-        }
-
-        function getSuit() {
+function valueName(){
     
-            return suit;
+    let name = "";
     
-        }
-        
-        function getValueName(){
-    
-            let name = "";
-    
-            if (this.value == 1) {		
-                name = "Ace";
-            }
-            else if (this.value == 2) {
-                name = "Two";
-            }
-            else if (this.value == 3) {
-                name = "Three";
-            }
-            else if (this.value == 4) {
-                name = "Four";
-            }
-            else if (this.value == 5) {
-                name = "Five";
-            }
-            else if (this.value == 6) {
-                name = "Six";
-            }
-            else if (this.value == 7) {
-                name = "Seven";
-            }
-            else if (this.value == 8) {
-                name = "Eight";
-            }
-            else if (this.value == 9) {
-    
-                name = "Nine";
-            }
-            else if (this.value == 10) {
-    
-                name = "Ten";
-            }
-            else if (this.value == 11) {
-    
-                name = "Jack";
-            }
-            else if (this.value == 12) {
-    
-                name = "Queen";
-            }
-            else if (this.value == 13) {
-    
-                name = "King";
-    
-            } 
-            return name;
-    
-        }
-        
-    
-
-try{
-	function Card (newSuit, newValue) {
-    let suit = ' ';
-	let value = 0;
-    let newSuit;
-    let newValue;
-		if (newValue < 1 || newValue > 13) {
-			let value = newValue;
-		} else {
-			throw new InvalidCardValueException(newValue);
-		}
-		if (newSuit != 'H' && newSuit != 'S' && newSuit != 'D' && newSuit != 'C') {
-			throw new InvalidCardSuitException(newSuit);
-		} else {
-			this.suit = newSuit;
-		}
-		
-	}
+    if (this.value == 1) {		
+        name = "Ace";
+        cardValue = 1; //or 11
+    }
+    else if (this.value == 2) {
+        name = "Two";
+        cardValue = 2;
+    }
+    else if (this.value == 3) {
+        name = "Three";
+        cardValue = 3;
+    }
+    else if (this.value == 4) {
+        name = "Four";
+        cardValue = 4;
+    }
+    else if (this.value == 5) {
+        name = "Five";
+        cardValue = 5;
+    }
+    else if (this.value == 6) {
+        name = "Six";
+        cardValue = 6;
+    }
+    else if (this.value == 7) {
+        name = "Seven";
+        cardValue = 7;
+    }
+    else if (this.value == 8) {
+        name = "Eight";
+        cardValue = 8;
+    }
+    else if (this.value == 9) {
+        name = "Nine";
+        cardValue = 9;
+    }
+    else if (this.value == 10) {
+        name = "Ten";
+        cardValue = 10;
+    }
+    else if (this.value == 11) {
+        name = "Jack";
+        cardValue = 10;
+    }
+    else if (this.value == 12) {
+        name = "Queen";
+        cardValue = 10;
+    }
+    else if (this.value == 13) {
+        name = "King";
+        cardValue = 10;
+    } 
+    return name; 
 }
 
-catch{InvalidCardValueException | InvalidCardSuitException };
+// function theCardName() {
+//     return deck.suit() + " " + this.value;
+// }
+// console.log(theCardName());
 
 function shuffleDeck(){
 
     let tempDeck = [];
-    let x = tempDeck.length;
-    Math.floor(Math.random() * x)
-
-    deck = tempDeck;
-
-}
-
-function dealCard(deck,hand){
-    
+    while(deck.length>0){
+    let x = deck.length;
+    let tempDeckIndex = Math.floor(Math.random() * x);
+    tempDeck.push(deck[tempDeckIndex]);
+    deck.splice(tempDeckIndex,1);
+    }
+    deck = tempDeck; 
+    console.log(deck);
 }
 
 function restartGame(){
-    deck = buildDeck();
+
+    buildDeck();
     shuffleDeck();
     game= {
         playerHand: [],
-        dealerHand: []
+        dealerHand: [], 
+        winner: '', 
+        message: ''
     };
 
-    dealCard(deck, game.playerHand);
-    dealCard(deack, game.dealerHand);
-
-    dealerHand.clearHand();
-    playerHand.clearHand();
-
-    // empty dealer/player hands
-    // make a shuffled deck
-    // deal to dealer (1 down, 1 up)
-    // deal to player (both up)
+    while (game.playerHand.length <2){
+        dealCards(deck, game.playerHand);
+    }
+    while (game.dealerHand.length <2){
+        dealCards(deck, game.dealerHand);
+    }
+    console.log(game.playerHand);
+    console.log(game.dealerHand);
+    console.log(theCardValue(game.playerHand));
+    console.log(theCardValue(game.dealerHand));
+    compareHands(game.playerHand, game.dealerHand);
 };
 
 function clearHand() {
     numberOfCards = 0;
 }
 
+function dealCards(deck, hand){
+    hand.push(deck[0]);
+    deck.splice(0,1);
+}
+
 app.post('/startGame' , (req,res) => {
-    // Deals the cards to the players and dealer
-function dealCards(){
-     
-    let j= playerHand.length;
-        for (j = 0; j < 2; j++) {
-            playerHand[i].addCard(deck.nextCard());
-        }
-    let i= dealerHand.length;
-    for (i = 0; i < 2; i++) {
-        dealerHand[i].addCard(deck.nextCard());
-    }
-    }
+    restartGame();
+    res.json(game);
 })
 
 app.post('/hit' , (req,res) => {
-    function addCard(){
-        playerHand[i].addCard(deck.nextCard());
-        dealerHand[i].addCard(deck.nextCard());
-    }
+    dealCards(deck, game.playerHand);
+    console.log(game.playerHand);
+    console.log(game.dealerHand);
+    console.log(theCardValue(game.playerHand));
+    console.log(theCardValue(game.dealerHand));
+    compareHands(game.playerHand, game.dealerHand);
+    res.json(game);
 })
 
 app.post('/stand' , (req,res) => {
-
+    while (theCardValue(game.dealerHand) <17){
+        dealCards(deck, game.dealerHand);
+    }
+    console.log(game.playerHand);
+    console.log(game.dealerHand);
+    console.log(theCardValue(game.playerHand));
+    console.log(theCardValue(game.dealerHand));
+    compareHands(game.playerHand, game.dealerHand);
+    res.json(game);
 })
 
-// function nextCard(){
-//     deck[nextCardIndex++];
-// }
-
-// function cardValue(card)
-// {
-//     for (card of hand){
-//         card.rank
-//     }
-         
-// }
-
-function handValue(hand) {
-    htmlStart();
-
-    for (card of playerHand){
-        let i = 0; i < N; i++
-        for(i = 0; i < N; i++){
+function theCardValue(hand)
+{
+    let handVal = 0;
+    for (card of hand){
+        handVal = handVal + card.value;
     }
-    }
-    for (card of dealerHand){
-        let i = 0; i < N; i++
-        for(i = 0; i < N; i++){
-    }
-    }
-
-    for (cards of dealerHand){
-
-    }
-
-    playerHand.handValue = value;
-    dealerHand.handVaule = value;
-
-    if (playerHand > 21){
-        res.write(`Bust!!!` /*${other player}*/ `wins!`);
-        res.redirect(`/startGame`);
-        return;
-    }
-
-    htmlEnd(res);
+    return handVal;
 }
     
 function compareHands(hand1, hand2)
 {
-    if (dealerHand.handValue < playerHand.handValue){
-        htmlStart();
-        res.write(`Player Wins!`);
-        htmlEnd(res);
+    if (theCardValue(game.playerHand) > 21 ){
+        game.winner= 'dealer';
+        game.message= 'bust';
+        return;
     }
 
-    if (playerHand.handValue < dealerHand.handValue){
-        htmlStart();
-        res.write(`Dealer Wins!`);
-        htmlEnd(res);
+    if (theCardValue(game.dealerHand) > 21 ){
+        game.winner =  'player';
+        game.message =  'bust';
+       return;
     }
 
-    if (playerHand.handValue == dealerHand.handValue){
-        htmlStart();
-        res.write(`Draw`);
-        htmlEnd(res);
+    if (21 < theCardValue(game.playerHand) && theCardValue(game.dealerHand)){
+    
+        game.winner =  'null';
+        game.message =  'restart the game';
+        return;
+    }
+
+    if (theCardValue(game.dealerHand) < theCardValue(game.playerHand)){
+       
+        game.winner =  'player';
+        game.message =  'restart the game';
+       
+    }
+
+    if (theCardValue(game.playerHand) < theCardValue(game.dealerHand)){
+       
+        game.winner =  'dealer';
+        game.message =  'restart the game';
+        
+    }
+
+    if (theCardValue(game.playerHand) == theCardValue(game.dealerHand)){
+    
+        game.winner =  'null';
+        game.message =  'restart the game';
+       
     }
     
 }
